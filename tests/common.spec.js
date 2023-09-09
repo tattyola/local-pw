ac15a2d5725badb8d029648a527ad1d79de34b6d
Tatsiana A <astrouskaya@yahoo.com>
2023-09-08 23:28:27 -0400

    added prettier

--- tests/common.spec.js
+++ tests/common.spec.js
@@ -1,38 +1,36 @@
-import { expect, test } from "@playwright/test";
-import { logIn } from "../common/log-in";
+import {expect, test} from '@playwright/test'
+import {logIn} from '../common/log-in'
 
 test.describe('Common', () => {
-    test.beforeEach(async ({ page }, testInfo) => {
-        await logIn(page, process.env.EMAIL, process.env.PASSWORD)
-    });
+  test.beforeEach(async ({page}, testInfo) => {
+    await logIn(page, process.env.EMAIL, process.env.PASSWORD)
+  })
 
-    test('Navigation', async ({page}) => {
-        await expect(page.locator('.ant-avatar-square')).toBeVisible();
-        await expect(page.locator('h1')).toBeVisible();
-        await expect(page.locator('h1')).toHaveText('Tatsiana Astrouskaya');
+  test('Navigation', async ({page}) => {
+    await expect(page.locator('.ant-avatar-square')).toBeVisible()
+    await expect(page.locator('h1')).toBeVisible()
+    await expect(page.locator('h1')).toHaveText('Tatsiana Astrouskaya')
 
-        await page.getByTestId("topmenu-Курсы").click();
-        await expect(page.getByText('Курсы программирования и тестирования')).toBeVisible();
-        await expect(page.locator('h1')).toHaveText('Курсы программирования и тестирования');
-        await expect(page).toHaveURL('/course');
+    await page.getByTestId('topmenu-Курсы').click()
+    await expect(page.getByText('Курсы программирования и тестирования')).toBeVisible()
+    await expect(page.locator('h1')).toHaveText('Курсы программирования и тестирования')
+    await expect(page).toHaveURL('/course')
 
-        await page.getByTestId("topmenu-Задачи").click();
-        await expect(page.getByText('Кодинг задачи')).toBeVisible();
-        await expect(page.locator('.h1')).toHaveText('Кодинг задачи');
-        await expect(page).toHaveURL('/challenge?limit=30&page=1');
+    await page.getByTestId('topmenu-Задачи').click()
+    await expect(page.getByText('Кодинг задачи')).toBeVisible()
+    await expect(page.locator('.h1')).toHaveText('Кодинг задачи')
+    await expect(page).toHaveURL('/challenge?limit=30&page=1')
 
-        await page.getByTestId("topmenu-Интервью").click();
-        await expect(page.getByText('Interview practice cards')).toBeVisible();
-        await expect(page.locator('h1')).toHaveText('Interview practice cards');
-        await expect(page).toHaveURL('/flash');
+    await page.getByTestId('topmenu-Интервью').click()
+    await expect(page.getByText('Interview practice cards')).toBeVisible()
+    await expect(page.locator('h1')).toHaveText('Interview practice cards')
+    await expect(page).toHaveURL('/flash')
 
-        await page.getByTestId("topmenu-Дневник").click();
-        await expect(page.getByText('Daily reports')).toBeVisible();
-        await expect(page.locator('h1')).toHaveText('Daily reports');
-        await expect(page).toHaveURL('/diary?page=1');
+    await page.getByTestId('topmenu-Дневник').click()
+    await expect(page.getByText('Daily reports')).toBeVisible()
+    await expect(page.locator('h1')).toHaveText('Daily reports')
+    await expect(page).toHaveURL('/diary?page=1')
 
-        await expect(page.locator('[data-qa="topmenu-Группы"] a')).toBeVisible();
-
-
-    })
+    await expect(page.locator('[data-qa="topmenu-Группы"] a')).toBeVisible()
+  })
 })
