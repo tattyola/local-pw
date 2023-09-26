23d3a198a9534c164bb13476f11eabb38b0e9ecd
Tatsiana A <astrouskaya@yahoo.com>
2023-09-25 21:59:29 -0400

    add api intercept and fullfil with my data

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -6,17 +6,17 @@ test.describe('Authentication & Authorization', () => {
     await loginPage.open()
   })
 
-  test('Sign in with existing credentials', async ({ dashboardPage, loginPage }) => {
+  test('Sign in with existing credentials', async ({ profilePage, loginPage }) => {
     // await loginPage.logIn(process.env.EMAIL, process.env.PASSWORD)
 
     await loginPage.input.email.fill(process.env.EMAIL)
     await loginPage.input.password.fill(process.env.PASSWORD)
     await expect(loginPage.button.submit).toBeEnabled()
     await loginPage.button.submit.click()
-    await expect(dashboardPage.avatar).toBeVisible()
+    await expect(profilePage.avatar).toBeVisible()
   })
 
-  test('Sign in with wrong credentials', async ({ dashboardPage, loginPage }) => {
+  test('Sign in with wrong credentials', async ({ profilePage, loginPage }) => {
     // await loginPage.logIn('Fake@yahoo.com', 'FakePassword')
 
     await loginPage.input.email.fill('Fake@yahoo.com')
@@ -24,6 +24,6 @@ test.describe('Authentication & Authorization', () => {
     await loginPage.button.submit.click()
     await expect(loginPage.toast).toBeVisible()
     await expect(loginPage.toast).toHaveText('User login. Fail')
-    await expect(dashboardPage.avatar).not.toBeVisible()
+    await expect(profilePage.avatar).not.toBeVisible()
   })
 })
